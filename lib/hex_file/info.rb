e7a7e390396b1991d6f5b9aebc2733fd6e3c1abf
Eflukx <rogier@lodewijks.org>
2016-04-28 14:01:29 +0200

    Added HexFile::Info#to_s

--- lib/hex_file/info.rb
+++ lib/hex_file/info.rb
@@ -9,13 +9,19 @@ module HexFile
     def initialize(input)
       input = input.read if input.is_a? IO
 
+      @record_types = []
+
       @records = input.each_line.map do |record_line|
-        Record.new(record_line.strip)
+        record = Record.new(record_line.strip)
+        @record_types << record.type
+        record
       end
+
+      @record_types.uniq!
     end
 
     def binary_size type = :all
-      records(type).map { |r| r.data_size }.reduce(:+) if record_type
+      records(type).map { |r| r.data_size }.reduce(:+) if type
     end
 
     def records type = :all
@@ -41,7 +47,7 @@ module HexFile
     end
 
     def record_types
-      @record_types ||= records.map { |r| r.type }.uniq
+      @record_types
     end
 
     def has_linear_record_types?
@@ -51,6 +57,16 @@ module HexFile
 
     def has_start_segment_record_type?
       record_types.include?(Record::START_SEGMENT_ADDRESS)
+    end
+
+    def to_s
+      [
+          "#{self.class}:",
+          "format         #{format}",
+          record_types.map { |t| "records/lines (type: #{t}) #{records(t).count}" },
+          record_types.map { |t| "binary size   (type: #{t}) #{binary_size(t)}" },
+          "checksums      #{ok? ? 'ok' : 'FAILED'}"
+      ].join "\n"
     end
 
   end
